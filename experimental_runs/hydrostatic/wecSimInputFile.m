5154da809520be91c39b6003aa1c3771157c4f3d
cmichelen <cmichel@sandia.gov>
2015-11-18 16:35:44 -0700

    working on model setup

--- experimental_runs/hydrostatic/wecSimInputFile.m
+++ experimental_runs/hydrostatic/wecSimInputFile.m
@@ -1,7 +1,7 @@
 %% Simulation Data
 simu=simulationClass();
 simu.simMechanicsFile = 'FOSWEC.slx';
-simu.rampT = 0.001;
+simu.rampT = 0;
 simu.endTime = 30;
 simu.dt = 0.01;
 simu.mode = 'normal';
@@ -9,19 +9,14 @@ simu.explorer = 'on';
 simu.domainSize = 2;
 
 %% Wave Information
-waves = waveClass('noWave');
-waves.noWaveHydrodynamicCoeffT=2*pi/0.7222053;
-%waves.noWaveHydrodynamicCoeffT=2*pi/22.7812443;
+%waves = waveClass('noWave');
+%waves.noWaveHydrodynamicCoeffT=2*pi/0.7222053;
 %
 %waves = waveClass('regular');
 %waves.T=2*pi/0.7222053;
 %waves.H=0;
 %
-%waves = waveClass('regularCIC');
-%waves.T=2*pi/0.7222053;
-%waves.H=0;
-%
-%waves = waveClass('noWaveCIC')
+waves = waveClass('noWaveCIC');
 
 %% Body Data
 %% Body 1: Platform
@@ -50,6 +45,58 @@ body(3) = bodyClass('./hydroData/Analysis.h5',3);
 body(3).geometryFile = './geometry/flap.stl';
 body(3).mass = 13.5204596444964;     
 body(3).momOfInertia = [2.3664 1.0264 1.3736];
+
+%% Cyl Fixed
+cylFixed.mass = 1;
+cylFixed.momentOfInertia = [1 1 1];
+cylFixed.cg = [0 0 -1];
+cylFixed.dispVol = 0;
+cylFixed.initDisp.initLinDisp = [0 0 0];
+cylFixed.initDisp.initAngularDispAxis = [0 1 0];
+cylFixed.initDisp.initAngularDispAngle = 0;
+cylFixed.geometryFile = 'empty.stl';
+cylFixed.viz.color = [0 1 1];
+cylFixed.viz.opacity = 1;
+
+%% Cyl Heaving
+cyl.mass = 15;
+cyl.momentOfInertia = [1 1 1];
+cyl.cg = [-0.5];
+cyl.dispVol = 15/1000;
+cyl.initDisp.initLinDisp = [0 0 0];
+cyl.initDisp.initAngularDispAxis = [0 1 0];
+cyl.initDisp.initAngularDispAngle = 0;
+cyl.geometryFile = 'empty.stl';
+cyl.viz.color = [0 1 1];
+cyl.viz.opacity = 1;
+
+%% Squares Pitching
+squares.mass = 3;
+squares.momentOfInertia = [1 1 1];
+squares.cg = [0 0 0];
+squares.dispVol = 3/1000;
+squares.initDisp.initLinDisp = [0 0 0];
+squares.initDisp.initAngularDispAxis = [0 1 0];
+squares.initDisp.initAngularDispAngle = 0;
+squares.geometryFile = 'empty.stl';
+squares.viz.color = [0 1 1];
+squares.viz.opacity = 1;
+
+%% Rectangle Surging
+rectangleFrame.mass = 5;
+rectangleFrame.momentOfInertia = [1 1 1];
+rectangleFrame.cg = [0 0 0];
+rectangleFrame.dispVol = 5/1000;
+rectangleFrame.initDisp.initLinDisp = [0 0 0];
+rectangleFrame.initDisp.initAngularDispAxis = [0 1 0];
+rectangleFrame.initDisp.initAngularDispAngle = 0;
+rectangleFrame.geometryFile = 'empty.stl';
+rectangleFrame.viz.color = [0 1 1];
+rectangleFrame.viz.opacity = 1;
+
+
+
+
 
 %% PTO and Constraint Parameters
 %% Constraint 1
