b93249668c2a30e44be1c6287001eb8ba8a136cd
kmruehl <kmruehl@sandia.gov>
2015-11-20 15:37:36 -0800

    surgeDecayCases for 5,10,15,20,15cm offset

---
+++ experimental_runs/DecayTests/SurgeDecay/userDefinedFunctionsMCR.m
@@ -0,0 +1,1 @@
+movefile('output',['surgeDecayCase_',num2str(mcr.cases(imcr)*100,'%2g'),'cm'])
--- experimental_runs/DecayTests/SurgeDecay/wecSimInputFile.m
+++ experimental_runs/DecayTests/SurgeDecay/wecSimInputFile.m
@@ -8,15 +8,9 @@ simu.mode = 'normal';
 simu.explorer = 'on';
 simu.domainSize = 2;
 simu.CITime = 10;
+simu.mcrCaseFile = 'surgeDecayCase.mat';
 
 %% Wave Information
-%waves = waveClass('noWave');
-%waves.noWaveHydrodynamicCoeffT=2*pi/0.7222053;
-%
-% waves = waveClass('regular');
-% waves.T=2;
-% waves.H=0.1;
-%
 waves = waveClass('noWaveCIC');
 
 %% Body Data
@@ -27,29 +21,21 @@ body(1).mass = 79.437181353569;
 body(1).momOfInertia = [57.0431 47.211 66.2529];
 body(1).viz.color = [1 1 1];
 body(1).viz.opacity = 0.25;
-body(1).initDisp.initLinDisp=[0.25,0,0];
+% body(1).initDisp.initLinDisp=[0.25,0,0];
 
 %% Body 2: Front Flap
 body(2) = bodyClass('../../hydroData/Analysis.h5',2);                     
 body(2).geometryFile = '../../geometry/flap.stl';
 body(2).mass = 13.5204596444964;     
 body(2).momOfInertia = [2.3664 1.0264 1.3736];
-body(2).initDisp.initLinDisp=[0.25,0,0];
-
-%body(2).linearDamping(5) = 10;  
-%alpha = 15*pi/180;
-%r = 0.5 - 0.338;
-%body(2).initDisp.initLinDisp = [r*sin(alpha),0,-1*r*(1-cos(alpha))];
-%body(2).initDisp.initAngularDispAxis = [0 1 0];
-%body(2).initDisp.initAngularDispAngle = alpha;
-%clear r alpha
+% body(2).initDisp.initLinDisp=[0.25,0,0];
 
 %% Body 3: Back Flap
 body(3) = bodyClass('../../hydroData/Analysis.h5',3);                     
 body(3).geometryFile = '../../geometry/flap.stl';
 body(3).mass = 13.5204596444964;     
 body(3).momOfInertia = [2.3664 1.0264 1.3736];
-body(3).initDisp.initLinDisp=[0.25,0,0];
+% body(3).initDisp.initLinDisp=[0.25,0,0];
 
 %% Cyl Fixed
 cylFixed.mass = 1;
